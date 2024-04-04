c613b21b0152435e8d784e9b182707446528c4ac
Jonah Ramponi <jonahramponi@gmail.com>
2024-04-04 14:37:36 +0100

    what linting are you using matt ? and adding page config

--- src/legal_nlp/frontend/config.py
+++ src/legal_nlp/frontend/config.py
@@ -1,10 +1,21 @@
 import os
 
+
 class Config:
     # Default values
-    NLP_CONNECTION_STRING = os.getenv("NLP_API_CONNECTION_STRING", "http://127.0.0.1:8542")
+    NLP_CONNECTION_STRING = os.getenv(
+        "NLP_API_CONNECTION_STRING", "http://127.0.0.1:8542"
+    )
     RUNPOD_BEARER_TOKEN = os.getenv("RUNPOD_BEARER_TOKEN", "")
-    RUNPOD_BASE_URI = os.getenv("RUNPOD_BASE_URI", "https://api.runpod.ai/v2/5bniip0yjm37iq")
+    RUNPOD_BASE_URI = os.getenv(
+        "RUNPOD_BASE_URI", "https://api.runpod.ai/v2/5bniip0yjm37iq"
+    )
     STREAM_CHAT = os.getenv("STREAM_CHAT", True)
     RUNPOD_STREAM_DELAY = os.getenv("RUNPOD_STREAM_DELAY", 0.0)
     RUNPOD_STATUS_CHECK_DELAY = os.getenv("RUNPOD_STATUS_CHECK_DELAY", 0.1)
+
+
+class PageConfig:
+    page_title = "SCOTi"
+    page_icon = "frontend/static/images/scoti_logo.png"
+    layout = "wide"
