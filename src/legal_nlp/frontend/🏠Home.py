558465461e78c78b7e04850d27776979edf86a74
Jonah Ramponi <jonahramponi@gmail.com>
2024-04-04 14:36:39 +0100

    use pageconfig + erica's materials

--- src/legal_nlp/frontend/🏠Home.py
+++ src/legal_nlp/frontend/🏠Home.py
@@ -1,9 +1,12 @@
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
 
+from config import PageConfig
+
 st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
+    layout=PageConfig.layout,
+    page_title=PageConfig.page_title,
+    page_icon=PageConfig.page_icon,
 )
 
 add_logo("frontend/static/images/smartR-AI-logo-RGB_250x90.png", height=65)
@@ -14,6 +17,21 @@ st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
-    SCOTi is ...
+SCOTi™ AI,  your loyal companion by smartR AI, is a powerful, customized AI 
+assistant that helps organizations unlock the value in their data. It is built 
+using state-of-the-art large language models that can understand context 
+and generate human-like responses.
+
+### Key Benefits
+
+| **Benefit**     | **Details**      |
+| ------------- | ------------- |
+| Data privacy and security | SCOTi keeps data safe and confidential. It is self-hosted for extra security.|
+| Cost savings | SCOTi requires far less computing power than other models, saving significantly on costs.|
+| Improved efficiency | SCOTi frees up employees from menial tasks so they can focus on more strategic initiatives. This leads to major productivity and efficiency gains. |
+| Personalization | SCOTi is customized for each client's unique needs. This ensures high-quality, relevant results tailored to each business. |
+| Time savings | SCOTi has extraordinary data and information retrieval abilities, saving teams huge amounts of time digging for information. |
+| Easy-to-understand | SCOTi communicates insights from data analysis in straightforward English anyone can understand. No coding or data science expertise required. |
+| Ownership | When SCOTi gets trained for you, you own the final model. That’s a refreshing change from the SaaS and license fees you have to pay now |
 """
 )
