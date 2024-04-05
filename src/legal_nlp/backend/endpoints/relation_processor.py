8e2c417ee95e5523d3c75c8a23b98f324aaa2a9c
Jonah Ramponi <jonahramponi@gmail.com>
2024-04-05 18:06:00 +0100

    minor prompt tweak

--- src/legal_nlp/backend/endpoints/relation_processor.py
+++ src/legal_nlp/backend/endpoints/relation_processor.py
@@ -9,7 +9,9 @@ import html
 class RelationProcessor:
     def __init__(self):
         self.gpt_client = RunpodClient()
-        self.tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1")
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "mistralai/Mixtral-8x7B-Instruct-v0.1"
+        )
 
     def extract_json_from_text(self, text):
         # Define a regular expression pattern to match each dictionary
@@ -20,40 +22,47 @@ class RelationProcessor:
         return json.loads(f'[{",".join(matches)}]')
 
     def strip_angle_brackets(self, entity):
-        parts = entity.split('>')
-        return parts[-1] if len(parts) == 1 else parts[1].split('<')[0]
+        parts = entity.split(">")
+        return parts[-1] if len(parts) == 1 else parts[1].split("<")[0]
 
     def json_to_dot(self, json_data):
-        """ 
-            Convert json entity relation into a graphviz DOT graph
+        """
+        Convert json entity relation into a graphviz DOT graph
         """
         dot = "digraph G {\n"
 
         for item in json_data:
-            relation = item['relation']
-            entity1 = self.strip_angle_brackets(item['entity1']['entity'])
-            entity2 = self.strip_angle_brackets(item['entity2']['entity'])
-            description = html.escape(item['additional_info']['description'])
-            
+            relation = item["relation"]
+            entity1 = self.strip_angle_brackets(item["entity1"]["entity"])
+            entity2 = self.strip_angle_brackets(item["entity2"]["entity"])
+            description = html.escape(item["additional_info"]["description"])
+
             dot += f'"{entity1}" -> "{entity2}" [label="{relation}" tooltip="{description}" labeltooltip="{description}"];\n'
-            
+
         dot += "}"
-        
+
         print(dot)
         return dot
 
-    def build_up_relation_graph(self, text: str, existing_relations: str, max_new_tokens: int):
+    def build_up_relation_graph(
+        self, text: str, existing_relations: str, max_new_tokens: int
+    ):
         """
-            This funciton is meant for iteratively building up the relation graph incrementally rather than all at once. Will return {"graph_svg": None, "relation_json": None} when finished
+        This funciton is meant for iteratively building up the relation graph incrementally rather than all at once. Will return {"graph_svg": None, "relation_json": None} when finished
         """
-        relation_graph_dict = self.get_relation_graph(text, existing_relations, max_new_tokens)
+        relation_graph_dict = self.get_relation_graph(
+            text, existing_relations, max_new_tokens
+        )
         return relation_graph_dict
 
-    def get_relation_graph(self, text: str, existing_relations: str = "", max_new_tokens: int = 2048):
+    def get_relation_graph(
+        self, text: str, existing_relations: str = "", max_new_tokens: int = 2048
+    ):
         generate_relations_json_prompt = """You are an expert entity relation extractor for legal documents. 
 You will be given a document with entities extracted using NLP. The extracted entities are represented using angled bracket tags, for example <DATE>17 December 2020</DATE> represents a detected date.
 Please extract ALL the relations between ALL the tagged entities in the text in a json format. The json should include the abbreviated relation, the relevant entity pair, and additional information about the relation.
 Make sure to capture the direction of the relationship as well, for example in the relation Carmichael Contracted_With OneSteel, Carmichael should be entity1 and OneSteel should be entity2.
+The relationships you define will be used to build a knowledge graph to inform key officials. You must be accurate, defining clear and consistent relationships.
 
 Here is a sample for the output json schema:
 ```json
@@ -69,28 +78,38 @@ Here is a sample for the output json schema:
 ```
 ONLY RESPOND IN JSON!
 Here is the text from which to extract the entity relations:\n"""
-        
+
         generate_relations_json_prompt += text
 
         messages = [{"role": "user", "content": generate_relations_json_prompt}]
-        
-        chat_prompt = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-        
+
+        chat_prompt = self.tokenizer.apply_chat_template(
+            messages, tokenize=False, add_generation_prompt=True
+        )
+
         # If we have an exisint relation dict that we're extending start off the assistant response with it
         existing_relations_prompt = ""
         if len(existing_relations) > 0:
-            indented_relations_dict = json.dumps(json.loads(existing_relations), indent=4)
+            indented_relations_dict = json.dumps(
+                json.loads(existing_relations), indent=4
+            )
             # Remove closing bracket and add comma to prompt model to continue
-            existing_relations_prompt = indented_relations_dict.split("]")[0].strip() + ","
+            existing_relations_prompt = (
+                indented_relations_dict.split("]")[0].strip() + ","
+            )
             chat_prompt += " " + existing_relations_prompt
 
-        gpt_response = self.gpt_client.get_gpt_response({}, generation_args={"max_tokens": max_new_tokens}, prompt=chat_prompt)
-        
+        gpt_response = self.gpt_client.get_gpt_response(
+            {}, generation_args={"max_tokens": max_new_tokens}, prompt=chat_prompt
+        )
+
         # Assume that scoti will return the entities wrapped in square brackets
-        relations_json = self.extract_json_from_text(existing_relations_prompt + gpt_response)
+        relations_json = self.extract_json_from_text(
+            existing_relations_prompt + gpt_response
+        )
         dot_graph = self.json_to_dot(relations_json)
-        
-        dot_graph = dot_graph.replace('}', 'rankdir="LR";}')
+
+        dot_graph = dot_graph.replace("}", 'rankdir="LR";}')
 
         # Render the graph from the DOT representation
         graph = graphviz.Source(dot_graph)
@@ -99,5 +118,6 @@ Here is the text from which to extract the entity relations:\n"""
         graph_svg = graph.pipe(format="svg").decode("utf-8").strip()
 
         return {"graph_svg": graph_svg, "relation_json": relations_json}
+
 
 relation_processor = RelationProcessor()
