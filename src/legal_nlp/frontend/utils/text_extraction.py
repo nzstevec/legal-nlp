3297905eff9853af0c85142d57027da58d1b7b98
Jonah Ramponi <jonahramponi@gmail.com>
2024-04-05 13:11:19 +0100

    clean up + support for rtf

--- src/legal_nlp/frontend/utils/text_extraction.py
+++ src/legal_nlp/frontend/utils/text_extraction.py
@@ -1,8 +1,8 @@
-from typing import List
-
 import docx
 import fitz
+
 from io import BytesIO, StringIO
+from striprtf.striprtf import rtf_to_text
 
 
 def load_file_contents(file_objects) -> str:
@@ -12,6 +12,7 @@ def load_file_contents(file_objects) -> str:
 
     output_contents = []
     for file_object in file_objects:
+        # Extract filename from Streamlit UploadFile object
         filepath = file_object.name
 
         if filepath.endswith(".txt"):
@@ -21,6 +22,10 @@ def load_file_contents(file_objects) -> str:
             doc = docx.Document(docx_file)
             text_chunks = [f"{para.text}\n" for para in doc.paragraphs]
             text = "".join(text_chunks)
+
+        elif filepath.endswith(".rtf"):
+            text = rtf_to_text(file_object.read().decode("utf-8"))
+
         elif filepath.endswith(".pdf"):
             pdf_document = fitz.open(stream=file_object.getvalue(), filetype="pdf")
             n_pages = len(pdf_document)
@@ -37,45 +42,3 @@ def load_file_contents(file_objects) -> str:
         output_contents.append(f"Filename: {filepath}\n\n{text}\n\n")
 
     return "".join(output_contents)
-
-
-def combine_prompt_with_file_contents(
-    prompt: str, extra_content: List[dict] = [], max_prompt_length: int = 8096
-):
-    """
-    Accepts extra_content like:
-    [
-        {
-            "filepath": ,
-            "contents":
-        },
-    ]
-
-    and a prompt like 'can you tell me about XYZ'
-
-    returns this combined into one message
-    """
-
-    if extra_content:
-        file_contents = [
-            "The following files have been provided by the user, to be used to help you generate a response.\n"
-        ]
-
-        for file_dict in extra_content:
-            file_desc = f"The file named: {file_dict['filepath']} has the content:\n{file_dict['contents']}\n\n"
-            file_contents.append(file_desc)
-
-        file_contents.append(
-            "You will use these files to clearly answer the users question."
-        )
-
-        file_contents_str = "".join(file_contents)
-    else:
-        file_contents_str = ""
-
-    scoti_legal_nlp_prompt = ""
-    final_prompt = scoti_legal_nlp_prompt.replace("<user-prompt>", prompt).replace(
-        "<file_contents>", file_contents_str
-    )
-
-    return final_prompt
