07cbc8ececfa13f16afbf28c96fd4d7158eaeb32
MatthewMalekP <matthewmalekp@gmail.com>
2024-04-16 00:55:44 +0100

    stream ws

--- src/legal_nlp/frontend/config.py
+++ src/legal_nlp/frontend/config.py
@@ -11,12 +11,12 @@ class Config:
         "RUNPOD_BASE_URI", "https://api.runpod.ai/v2/hjxnt6qh1tc7mp"
     )
     INFERENCE_CHAT_URI = os.getenv(
-        "INFERENCE_BASE_URI", "http://127.0.0.1:2235"
+        "INFERENCE_CHAT_URI", "http://127.0.0.1:2235"
     )
     INFERENCE_STREAM_URI = os.getenv(
-        "INFERENCE_BASE_URI", "http://127.0.0.1:2234"
+        "INFERENCE_STREAM_URI", "ws://127.0.0.1:2234"
     )
-    STREAM_CHAT = os.getenv("STREAM_CHAT", False)
+    STREAM_CHAT = os.getenv("STREAM_CHAT", True)
     RUNPOD_STREAM_DELAY = os.getenv("RUNPOD_STREAM_DELAY", 0.0)
     RUNPOD_STATUS_CHECK_DELAY = os.getenv("RUNPOD_STATUS_CHECK_DELAY", 0.1)
     RUNPOD_SERVERLESS=False
