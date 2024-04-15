be8b74363008432f2583d759f15784106ed9bfb8
MatthewMalekP <matthewmalekp@gmail.com>
2024-04-15 23:45:54 +0100

    Add missing config

--- src/legal_nlp/frontend/config.py
+++ src/legal_nlp/frontend/config.py
@@ -10,6 +10,12 @@ class Config:
     RUNPOD_BASE_URI = os.getenv(
         "RUNPOD_BASE_URI", "https://api.runpod.ai/v2/hjxnt6qh1tc7mp"
     )
+    INFERENCE_CHAT_URI = os.getenv(
+        "INFERENCE_BASE_URI", "http://127.0.0.1:2235"
+    )
+    INFERENCE_STREAM_URI = os.getenv(
+        "INFERENCE_BASE_URI", "http://127.0.0.1:2234"
+    )
     STREAM_CHAT = os.getenv("STREAM_CHAT", False)
     RUNPOD_STREAM_DELAY = os.getenv("RUNPOD_STREAM_DELAY", 0.0)
     RUNPOD_STATUS_CHECK_DELAY = os.getenv("RUNPOD_STATUS_CHECK_DELAY", 0.1)
